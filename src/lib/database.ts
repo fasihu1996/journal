2b8583768c1edb3f1de342f49fc5e1ef4a40a969
JonEffingSnow <fasih1996@gmail.com>
2025-07-01 15:44:08 +0200

    removed client import

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,12 +1,13 @@
 import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "@libsql/client";
 import { config } from "dotenv";
 
 if (!process.env.VERCEL) {
     config({ path: ".env.local" });
 }
 
-export const db = drizzle({ connection: {
-    url: process.env.TURSO_CONNECTION_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!,
-);
+export const db = drizzle({
+    connection: {
+        url: process.env.TURSO_CONNECTION_URL!,
+        authToken: process.env.TURSO_AUTH_TOKEN!,
+    },
+});
