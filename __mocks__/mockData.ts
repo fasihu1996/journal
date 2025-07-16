11be906320c759a17d8a9002d19059fb58d4abf8
JonEffingSnow <fasih1996@gmail.com>
2025-07-16 18:22:50 +0200

    mock data created for testing

---
+++ __mocks__/mockData.ts
@@ -0,0 +1,29 @@
+export const mockEntries = [
+  {
+    id: 1,
+    title: "Middle Entry",
+    content: "Middle Content",
+    mood: "okay",
+    favorited: false,
+    createdAt: "2025-07-16T13:17:17.485Z",
+    tags: ["tag1", "tag2", "tag3"],
+  },
+  {
+    id: 2,
+    title: "Newest Entry",
+    content: "Newest Content",
+    mood: "great",
+    favorited: true,
+    createdAt: "2025-07-16T13:35:27.075Z",
+    tags: [],
+  },
+  {
+    id: 3,
+    title: "Oldest Entry",
+    content: "Oldest Content",
+    mood: "terrible",
+    favorited: true,
+    createdAt: "2025-07-14T13:35:27.075Z",
+    tags: [],
+  },
+];
