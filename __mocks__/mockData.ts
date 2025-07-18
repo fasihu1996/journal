3ead657aa8ae16bf87b3658ddc88ccb6d4b66279
JonEffingSnow <fasih1996@gmail.com>
2025-07-18 16:31:09 +0200

    fixed order of entries

--- __mocks__/mockData.ts
+++ __mocks__/mockData.ts
@@ -1,14 +1,5 @@
 export const mockEntries = [
   {
-    id: 1,
-    title: "Middle Entry",
-    content: "Middle Content",
-    mood: "okay",
-    favorited: false,
-    createdAt: "2025-07-16T13:17:17.485Z",
-    tags: ["tag1", "tag2", "tag3"],
-  },
-  {
     id: 2,
     title: "Newest Entry",
     content: "Newest Content",
@@ -16,6 +7,15 @@ export const mockEntries = [
     favorited: true,
     createdAt: "2025-07-16T13:35:27.075Z",
     tags: [],
+  },
+  {
+    id: 1,
+    title: "Middle Entry",
+    content: "Middle Content",
+    mood: "okay",
+    favorited: false,
+    createdAt: "2025-07-16T13:17:17.485Z",
+    tags: ["tag1", "tag2", "tag3"],
   },
   {
     id: 3,
