f5b86af4e9011772f868fabd4c9581b977a65b55
fasihu1996 <fasih1996@gmail.com>
2025-07-19 20:56:01 +0200

    added tests for all required attributes

--- __tests__/entries-list.test.jsx
+++ __tests__/entries-list.test.jsx
@@ -13,16 +13,36 @@ jest.mock("@/lib/api", () => ({
 }));
 
 describe("Mandatory feature 1: display all entries", () => {
-  it("shows newest entry first", async () => {
+  beforeEach(() => {
     entriesApi.getEntries.mockResolvedValue(mockEntries);
-
+  });
+  it("shows the titles from newest to oldest", async () => {
     render(<Page />);
-
     await waitFor(() => {
       const titles = screen.getAllByRole("heading", { level: 3 });
       expect(titles[0]).toHaveTextContent("Newest Entry");
       expect(titles[1]).toHaveTextContent("Middle Entry");
       expect(titles[2]).toHaveTextContent("Oldest Entry");
+    });
+  });
+
+  it("shows the content", async () => {
+    render(<Page />);
+    await waitFor(() => {
+      const contents = screen.getAllByLabelText("content");
+      expect(contents[0]).toHaveTextContent("Newest Content");
+      expect(contents[1]).toHaveTextContent("Middle Content");
+      expect(contents[2]).toHaveTextContent("Oldest Content");
+    });
+  });
+
+  it("shows the time stamp", async () => {
+    render(<Page />);
+    await waitFor(() => {
+      const timestamps = screen.getAllByLabelText("datetime");
+      expect(timestamps[0]).toHaveTextContent("16. Juli 2025, 13:35");
+      expect(timestamps[1]).toHaveTextContent("16. Juli 2025, 13:17");
+      expect(timestamps[2]).toHaveTextContent("14. Juli 2025, 13:35");
     });
   });
 });
